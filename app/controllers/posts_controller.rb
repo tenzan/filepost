62862076eaf001fc62df74accdef3817112493b3
Askar Karasaev <askar.karasaev@gmail.com>
2021-11-29 21:33:09 +0900

    Adding post in conversation WIP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_conversation
 
@@ -9,19 +9,15 @@ class PostController < ApplicationController
        respond_to do |format|
            
            if @post.save
-               # Send an email
-               format.turbo_stream { render turbo_stream: turbo_stream.replace("form", partial: "posts/form", locals: { conversation: @conversation, post: Post.new })}
+            # Send an email
+            format.turbo_stream { render turbo_stream: turbo_stream.replace("form", partial: "posts/form", locals: { conversation: @conversation, post: Post.new })}
            else
-            format.turbo_stream { render  turbo_stream: turbo_stream.replace("form", partial: "posts/form", locals: { conversation: @conversation, post: @post })}
+            format.turbo_stream { render turbo_stream: turbo_stream.replace("form", partial: "posts/form", locals: { conversation: @conversation, post: @post })}
            end
-           
        end
-        
     end
-    
 
     private
-
     def set_conversation
         @conversation = Conversation.find(params[:conversation_id])
     end
@@ -29,5 +25,4 @@ class PostController < ApplicationController
     def post_params
         params.require(:post).permit(:body)
     end
-    
 end
